904cd4664ba2563d9fe442381a9e83a1c0a95a9a
Matthew Edwards <m.edwards3@ncl.ac.uk>
2019-03-28 13:55:37 +0000

    changed package discription

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="0.0.1",
     author="Matthew Edwards",
     author_email="matt.edwards@newcastle.ac.uk",
-    description="This package is designed to teach text classification.",
+    description="Text classification",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mt-edwards/classy-text",
